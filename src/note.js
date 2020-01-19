57451a0b272de27c33afdfe74fb3179eb74be2d0
Taylor Piccarreto <tpic12@gmail.com>
2020-01-19 17:50:40 -0500

    date and homepage format

--- src/note.js
+++ src/note.js
@@ -3,11 +3,15 @@ import ApiContext from './context/ApiContext';
 import PropTypes from 'prop-types'
 import './note.css'
 
-function change(time) { var r = time.match(/^\s*([0-9]+)\s*-\s*([0-9]+)\s*-\s*([0-9]+)(.*)$/); 
+function change(time) { 
+
+let t = time.slice(0,10)
+  
+var r = t.match(/^\s*([0-9]+)\s*-\s*([0-9]+)\s*-\s*([0-9]+)(.*)$/); 
   
 return r[2]+"-"+r[3]+"-"+r[1]+r[4]; }
 
-console.log(change("2019-01-03 T00:00:00.000Z"));
+console.log(change("2019-01-03 03:00:00"));
 
 
 
@@ -20,7 +24,7 @@ export default class Note extends React.Component {
         .map((note) =>
         <div key={note.id}>
             <h2 className='noteTitle'>{note.name}</h2>
-            <h3 className='noteDate'>Modified: {note.modified}</h3>
+            <h3 className='noteDate'>Modified: {change(note.modified)}</h3>
             <p className='noteContent'>{note.content}</p>
             <button className='noteDeleteButton'type="button" onClick={()=>this.context.deletehandlenote(note.id).then(()=>{this.props.history.push('/')})}>delete</button>
         </div>
